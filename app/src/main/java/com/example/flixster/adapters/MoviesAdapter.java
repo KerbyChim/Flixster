3f0a33ebe6c0433758f5157426372ab3a5853bdc
KerbyChim <antoinekerby@gmail.com>
2019-07-13 21:35:58 -0400

    Second Commit

--- app/src/main/java/com/example/flixster/adapters/MoviesAdapter.java
+++ app/src/main/java/com/example/flixster/adapters/MoviesAdapter.java
@@ -1,6 +1,7 @@
 package com.example.flixster.adapters;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.util.Log;
 import android.view.ContextMenu;
 import android.view.LayoutInflater;
@@ -64,7 +65,14 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.ViewHolder
         public void bind(Movie movie) {
            tvTitle.setText(movie.getTitle());
            tvOverview.setText(movie.getOverview());
-           Glide.with(context).load(movie.getPosterPath()).into(ivPoster);
+           String imageUrl = movie.getPosterPath();
+           //Reference the backdrop path is in landscape
+            if (context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+                imageUrl = movie.getBackdropPath();
+            }
+
+            //String imageUrl = movie.getPosterPath();
+           Glide.with(context).load(imageUrl).into(ivPoster);
         }
     }
 }
